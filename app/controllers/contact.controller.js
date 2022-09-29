b6e3bf0d1cf60e7145f10c8a5927e88b5097cf17
bichphuong1012 <phuongb1906553@student.ctu.edu.vn>
2022-09-29 18:05:51 +0700

    Cai dat cac handler truy cap CSDL

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,3 +1,8 @@
+const ContactService = require("../services/contact.service");
+const MongoDB = require("../utils/mongodb.util");
+const ApiError = require("../api-error");
+
+
 exports.create = (req, res) => {
     res.send({message: "create handler"});
 };
@@ -24,4 +29,21 @@ exports.deleteAll = (req, res) => {
 
 exports.findAllFavorite = (req, res) => {
     res.send({message: "findAllFavorite handler"});
+};
+
+//Create and Save a new Contact
+exports.create = async (req, res, next) => {
+    if (!req.body?.name){
+        return next (new ApiError(400, "Name can not be empty"));
+    }
+
+    try {
+        const ContactService = new ContactService(MongoDB.client);
+        const document = await ContactService.create(req.body);
+        return res.send(document);
+    } catch (error) {
+        return next(
+            new ApiError(500, "An error occurred while creating the contact")
+        );
+    }
 };
